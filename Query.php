1119f92919cbc6ba89fa6644686a50f300591cff
Matt Nowack <mdnowack@gmail.com = Matt Nowack mdnowack@gmail.com>
2009-10-29 05:21:40 +0000

    phpdoc added to the main Query class

--- Query.php
+++ Query.php
@@ -8,10 +8,21 @@ class Query {
 	private static $adapter;
 	private static $schema;
 	
+	/**
+	 * Private constructor used by factory methods
+	 * @param string $sql [optional] SQL to initialize with
+	 * @return New Query object wrapping supplied sql statement
+	 */
 	private function __construct($sql = "") {
 		$this->sql = $sql;
 	}
 	
+	/**
+	 * Set up Prosper Query Engine
+	 * @param string $schema [optional] Default schema to apply to queries
+	 * @param string $type [optional] Database type, mysql, mssql, pgsql (postgre), and sqlite accepted, defaults to mysql.
+	 * @return null
+	 */
 	static function configure($schema = "", $type = "mysql") {
 		switch(trim(strtolower($type))) {
 			case 'mysql':
@@ -33,7 +44,96 @@ class Query {
 		}		
 		self::$schema = ($schema == "" ? "" : self::quote($schema));
 	} 
+
+	/**
+	 * Creates an alias string if alias is not empty
+	 * @param string $alias alias to create 
+	 * @return string template is " as self::quote($alias)"
+	 */
+	static function alias($alias) {
+		return ($alias == "" ? "" : " as " . self::quote($alias));
+	}
 	
+	/**
+	 * Properly quotes a potential column, allowing for compound table.column format
+	 * @param string $col column name, simple or compound
+	 * @return Properly quoted column
+	 */
+	static function column($col) {
+		if(strpos($col, '.') === false) {
+			$column = self::quote($col);
+		} else {
+			$parts = explode('.', $col);
+			foreach($parts as $part) {
+				$safeparts[] = self::quote($part);
+			}
+			$column = implode('.', $safeparts);
+		}
+		
+		return $column;
+		 
+	}
+	
+	/**
+	 * Properly quotes and scopes a potential table.
+	 * @param string $table table name 
+	 * @return Properly scoped and quoted tablename ex: Query::table('hello') => `schema`.`hello` (for mysql)
+	 */
+	static function table($table) {
+		return self::schema('.') . self::quote($table);
+	}
+	
+	/**
+	 * Retrieves the schema with an optional suffix
+	 * @param object $append [optional] If the schema exists this text will be appended, most useful for appending a dot (.)
+	 * @return 
+	 */
+	static function schema($append = "") {
+		return (self::$schema == "" ? "" : self::$schema . $append);
+	}
+	
+	/**
+	 * Quotes the given string using the appropriate adapter's quote function.
+	 * Used with schema object names
+	 * @param string $str the text to quote
+	 * @return Properly quoted text
+	 * @see BaseAdapter#quote($str)
+	 */	
+	static function quote($str) {
+		return self::$adapter->quote($str);
+	}
+	
+	/**
+	 * Escapes the given string using the appropriate adapter's escape function.
+	 * Used with values to be serialized
+	 * @param string $str the text to escape
+	 * @return Properly escaped text
+	 * @see BaseAdapter#escape($str)
+	 * @todo implement this properly
+	 */
+	static function escape($str) {
+		return $str;
+	}
+	
+	/**
+	 * Unescapes the given string using the approriate adapter's unescape function
+	 * Used with values to be deserialized
+	 * @param string $str the text to unescape
+	 * @return Properly unescaped text
+	 * @see BaseAdapter#unescape($str)
+	 * @todo implement this properly
+	 */
+	static function unescape($str) {
+		return $str;
+	}
+	
+	/**
+	 * Factory function that creates a select statement query object.
+	 * @param varargs [optional] If nothing is provided assumes 'select *' 
+	 * 			otherwise processes each argument as a column, 
+	 * 			array arguments are processed as any number of aliased columns where column_name => alias 
+	 * @return Select Statement Query Object
+	 */
 	static function select() {
 		if(func_num_args() == 0) {
 			$columns = "*";
@@ -53,50 +153,115 @@ class Query {
 		return new Query("select $columns");
 	}
 	
+	/**
+	 * Properly escapes a comparison expression.
+	 * @param string $expression Generic comparison expression
+	 * @return string The properly escaped comparison expression
+	 * @example Query::parseComparison("a<1") => '`a` < "1"' (for mysql)
+	 */
+	static function comparison($expression) {
+		$operators = array('<=', ">=", "!=", "<>", "<", ">", "=", "LIKE");
+		foreach($operators as $op) {
+			if(stripos($expression, $op) !== false) {
+				$parse = explode($op, $expression);
+				return self::column(trim($parse[0])) . $op . self::escape(trim($parse[1]));
+			}
+		}
+	}
+	
+	/**
+	 * Produces the logical "not" conjunction 
+	 * @param object $clause if Query then evaluated, otherwise treated as a comparison expresssion
+	 * @return Query object containing the appropriate sql snippet
+	 */
+	static function notClause($clause) {
+		if($clause instanceof Query) {
+			$sql = " NOT($clause)";
+		} else {
+			$sql = " NOT(" . self::comparison($clause) . ")";
+		} 
+		return new Query($sql);
+	}
+	
+	/**
+	 * Produces the "and" part of a coditional conjunction
+	 * @return Query object containing the appropriate sql snippet
+	 */
+	static function andClause() {
+		return self::conjunction(' AND ', func_get_args());
+	}
+	
+	/**
+	 * Produces the "or" part of a conditional conjunction
+	 * @return Query object containing the appropriate sql snippet
+	 */
+	static function orClause() {
+		return self::conjunction(' OR ', func_get_args());
+	}
+	
+	/**
+	 * Generic conjunction generation function
+	 * @param string $glue Text to join conjunctions together with
+	 * @param array $args Arguments to join together
+	 * @return Query object containing the appropriate sql snippet
+	 */
+	static function conjunction($glue, $args) {
+		foreach($args as $arg) {
+			if($arg instanceof Query) {
+				$parts[] = "($arg)";
+			} else {
+				$parts[] = self::comparison($arg);
+			}
+		}
+		$sql .= implode($glue, $parts);
+		
+		return new Query($sql);
+	}
+	
+	/**
+	 * Chained function for describing the table to pull from
+	 * @param string $table table name
+	 * @param string $alias [optional] alias
+	 * @return Query instance for further chaining
+	 */
 	function from($table, $alias = "") {
 		$this->sql .= " from " . self::table($table) . self::alias($alias);
 		return $this;
 	}
 	
+	/**
+	 * Chained function for describing a standard cartesian join
+	 * @param object $table table name
+	 * @param object $alias [optional] alias
+	 * @return Query instance for further chaining
+	 */
 	function join($table, $alias = "") {
 		$this->sql .= " join " . self::table($table) . self::alias($alias);
 	}
 	
+	/**
+	 * Chained function for describing the where conditions
+	 * @param object $clause if a string than treated as a simple codition (i.e. x > 3) 
+	 * 						 if a query instance treated as a complex logic operation
+	 * @return Query instance for further chaining
+	 */
+	function where($clause) {
+		if($clause instanceof Query) {
+			$this->sql .= " where $clause";
+		} else {
+			$this->sql .= ' where ' . self::comparison($clause);
+		}
+		return $this;
+	}
 	
+	/**
+	 * Automagic toString method, simply prints wrapped sql statement
+	 * @return Wrapped Sql Statement
+	 */
 	function __toString() {
 		return $this->sql;
 	}
 	
-	static function alias($alias) {
-		return ($alias == "" ? "" : " as " . self::quote($alias));
-	}
-	
-	static function column($col) {
-		if(strpos($col, '.') === false) {
-			$column = self::quote($col);
-		} else {
-			$parts = explode('.', $col);
-			foreach($parts as $part) {
-				$safeparts[] = self::quote($part);
-			}
-			$column = implode('.', $safeparts);
-		}
-		
-		return $column;
-		 
-	}
-	
-	static function table($table) {
-		return self::schema('.') . self::quote($table);
-	}
-	
-	static function schema($append = "") {
-		return (self::$schema == "" ? "" : self::$schema . $append);
-	}
-		
-	static function quote($str) {
-		return self::$adapter->quote($str);
-	}
 	
 }
 
--- test.php
+++ test.php
@@ -8,17 +8,7 @@
 		echo "<h2>$adapter test</h2>";
 		Query::configure('test', $adapter);
 		
-		echo "Select All:  <br />";
-		echo Query::select()->from('user');
-		
-		echo "<br /><br />Select Columns:  <br />";
-		echo Query::select('fname', 'lname')->from('user');
-		
-		echo "<br /><br />Select Alias Columns: <br />";
-		echo Query::select(array('fname' => 'First Name', 'lname' => 'Last Name'))->from('user', 'u');
-		
-		echo "<br /><br />Select Alias Table Columns: <br />";
-		echo Query::select(array('user.fname' => 'First Name', 'pref.email' => 'Email Preference'))->from('sys_user', 'user');
+		echo Query::select()->from('user')->where(Query::andClause('a<1', 'b LIKE 2', Query::orClause('c>=3', 'd!=4')));
 		
 		echo "<hr />";
 	}
