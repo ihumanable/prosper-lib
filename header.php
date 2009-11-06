700069cc9af558f4db6fe59fce4a3b4820670a00
Matt Nowack <mdnowack@gmail.com = Matt Nowack mdnowack@gmail.com>
2009-11-06 16:20:30 +0000

    added a header file and externalized the styles there

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>prosper</title>
+		<style type="text.css">
+		
+		div#content {
+			width: 600px;
+			margin-left: auto;
+			margin-right: auto;
+		}
+		
+		div.post {
+			width: 600px;
+			padding: 10px;
+			float: left;
+			clear: both;
+			border: 1px solid black;
+			margin-bottom: 10px;
+		}
+		
+		</style>
+	</head>
--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 	//Ok pull out all of the blog posts, newest first
 	$posts = Query::select()->from('blog')->order('timestamp')->execute();
 	
-	echo '<div style="width: 600px; margin-left: auto; margin-right: auto">';
+	echo '<div id="content">';
 	echo "<h1>Blog Posts</h1>";
 	
 	foreach($posts as $post) {
-		echo '<div style="border: 1px solid black; padding: 10px; float: left; clear: both; width: 600px; margin-bottom: 20px">';
+		echo '<div class="post">';
 			echo "<h2>#{$post['id']} - {$post['title']} @ {$post['timestamp']}</h2>";
 			echo "<p>{$post['content']}</p>";
 		echo '</div>';	
