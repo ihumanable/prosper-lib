6e2812447560d34a541290d2bdc114b4b562300d
Matt Nowack <mdnowack@gmail.com = Matt Nowack mdnowack@gmail.com>
2009-11-08 01:00:16 +0000

    Added Infrormix Adapter

---
+++ lib/adapters/InformixAdapter.php
@@ -0,0 +1,52 @@
+<?php
+namespace Prosper;
+
+class InformixAdapter extends BaseAdapter {
+	
+	/**
+	 * Establishes an Informix Adapter
+	 * @param string $username Database username
+	 * @param string $password Database password
+	 * @param string $hostname Database hostname
+	 * @param string $schema Database schema
+	 * @return New Adapter Instance
+	 */
+	function __construct($username, $password, $hostname, $schema) {
+		parent::__construct($username, $password, $hostname, $schema);
+		$database = $schema . "@" . $hostname;
+		$this->connection = ifx_connect($database, $username, $password);
+	}
+	
+	/**
+	 * @see BaseAdapter#platform_execute($sql) 
+	 */
+	function platform_execute($sql) {
+		return ifx_query($sql, $this->connection);
+	}
+	
+	/**
+	 * @see BaseAdapter#affected_rows($set) 
+	 */
+	function affected_rows($set) {
+		return ifx_affected_rows($set);
+	}
+	
+	/**
+	 * @see BaseAdapter#fetch_assoc($set) 
+	 */
+	function fetch_assoc($set) {
+		return ifx_fetch_Row($set, "NEXT");
+	}
+	
+	/**
+	 * @see BaseAdapter#cleanup($set)
+	 * @param object $set
+	 * @return 
+	 */
+	function cleanup($set) {
+		ifx_free_result($set);
+	}
+	
+} 
+
+?>
