3eb57dd1d2175622afaef28fe57800b626af239e
Matt Nowack <mdnowack@gmail.com = Matt Nowack mdnowack@gmail.com>
2009-11-08 01:26:26 +0000

    Added MaxDB Adapter

---
+++ lib/adapters/MaxDBAdapter.php
@@ -0,0 +1,55 @@
+<?php
+namespace Prosper;
+
+class MaxDBAdapter extends BaseAdapter {
+	
+	/**
+	 * Establishes a MaxDB Adapter
+	 * @param string $username Database username
+	 * @param string $password Database password
+	 * @param string $hostname Database hostname
+	 * @param string $schema Database schema
+	 * @return New Adapter Instance
+	 */
+	function __construct($username, $password, $hostname, $schema) {
+		parent::__construct($username, $password, $hostname, $schema);
+		$this->connection = new maxdb($hostname, $username, $password, $schema);
+	}
+	
+	/**
+	 * @see BaseAdapter#platform_execute($sql); 
+	 */
+	function platform_execute($sql) {
+		return $this->connection->query($sql);
+	}
+	
+	/**
+	 * @see BaseAdapter#affected_rows($set)
+	 */
+	function affected_rows($set) {
+		return $this->connection->affected_rows;
+	}
+	
+	/**
+	 * @see BaseAdapter#insert_id($set) 
+	 */
+	function insert_id($set) {
+		return $this->connection->insert_id;
+	}
+	
+	/**
+	 * @see BaseAdapter#fetch_assoc($set) 
+	 */
+	function fetch_assoc($set) {
+		return $set->fetch_assoc();
+	}
+	
+	/**
+	 * @see BaseAdapter#cleanup($set) 
+	 */
+	function cleanup($set) {
+		$set->free();
+	}
+	
+}
+?>
