57e8c7373e64784e1297610c4ad1fa2df2c3ec32
Matt Nowack <mdnowack@gmail.com>
2009-11-24 16:18:48 -0500

    Changed config.php to add some more comments and moved the time_ago function to utility.php

--- todo/config.php
+++ todo/config.php
@@ -1,41 +1,15 @@
 <?php
-	namespace Prosper;
-	define('PROSPER_PATH', '../lib/');
+	//Standard pathing definition
+  define('PROSPER_PATH', '../lib/');
 	
+	//Require Prosper
 	require_once PROSPER_PATH . 'adapters/_common_.php';
 	require_once PROSPER_PATH . 'Query.php';
 
-	Query::configure(Query::MYSQL_MODE, 'root', 'xamppdevpwd', 'localhost', 'test');
+  //Go and get the utility functions
+  require_once 'utility.php';
 
-	/**
-	 * Simple function that translates a timestamp from the past into a friendlier
-	 * number of somethings ago (ex: 15 minutes ago, 1 hour ago)
-	 * @param int timestamp unix timestamp to convert
-	 * @return string nicely formatted label	 	 
-	 */	 	  	
-	function time_ago($timestamp) {
-		$spans = array ( "year"   => 31536000,
-		                 "month"  =>  2592000,
-										 "week"   =>   604000,
-										 "day"    =>    86400,
-										 "hour"   =>     3600,
-										 "minute" =>       60,
-										 "second" =>        1	 );
-					
-		$diff = mktime() - $timestamp - 24;
-		$label = "second";
-		$span = 1;
-		foreach($spans as $key => $value) {
-			if($diff > $value) {
-				$label = $key;
-				$span = $value;
-				break;
-			}
-		}
-		
-		$amt = floor($diff / $span);	
-		
-		return $amt . " " . $label . ($amt != 1 ? "s" : "") . " ago";
-	}
+  //Configure prosper to connect to the database
+	Prosper\Query::configure(Prosper\Query::MYSQL_MODE, 'root', 'xamppdevpwd', 'localhost', 'test');
 
 ?>
---
+++ todo/utility.php
@@ -0,0 +1,34 @@
+<?php
+
+	/**
+	 * Simple function that translates a timestamp from the past into a friendlier
+	 * number of somethings ago (ex: 15 minutes ago, 1 hour ago)
+	 * @param int timestamp unix timestamp to convert
+	 * @return string nicely formatted label	 	 
+	 */	 	  	
+	function time_ago($timestamp) {
+		$spans = array ( "year"   => 31536000,
+		                 "month"  =>  2592000,
+										 "week"   =>   604000,
+										 "day"    =>    86400,
+										 "hour"   =>     3600,
+										 "minute" =>       60,
+										 "second" =>        1	 );
+					
+		$diff = mktime() - $timestamp - 24;
+		$label = "second";
+		$span = 1;
+		foreach($spans as $key => $value) {
+			if($diff > $value) {
+				$label = $key;
+				$span = $value;
+				break;
+			}
+		}
+		
+		$amt = floor($diff / $span);	
+		
+		return $amt . " " . $label . ($amt != 1 ? "s" : "") . " ago";
+	}
+
+?>
