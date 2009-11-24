c860e71b522805c5c3aa7d2171420381841ced36
Matt Nowack <mdnowack@gmail.com = Matt Nowack mdnowack@gmail.com>
2009-11-24 02:30:57 +0000

    updated prosper footer (footer.php) to point to the correct page

--- todo/footer.php
+++ todo/footer.php
@@ -1,5 +1,5 @@
 			<div id="footer">
-				<a href="http://www.ihumanable.com/prosper/">prosper</a> designed by <a href="http://www.ihumanable.com">matt nowack</a>
+				<a href="http://www.ihumanable.com/blog/prosper/">prosper</a> designed by <a href="http://www.ihumanable.com">matt nowack</a>
 			</div>
 		</div>
 	</body>
