2507407479cac71131c4b1d849f8ff350dad657e
Matt Nowack <mdnowack@gmail.com>
2009-12-07 08:19:43 -0500

    Instructions for the config.php

--- todo/config.php
+++ todo/config.php
@@ -9,10 +9,10 @@
   //Go and get the utility functions
   require_once 'utility.php';
   
-  //Configure prosper to connect to the database
+  //Configure prosper to connect to the database (uncomment the appropriate line and fill in with proper credentials)
   
   //MS-SQL Sample
-    //Prosper\Query::configure(Prosper\Query::MSSQL_MODE, 'sa', 'sa', 'SERVER\SQLEXPRESS', 'test.dbo');
+    //Prosper\Query::configure(Prosper\Query::MSSQL_MODE, 'sa', 'sa', 'LOCALHOST\SQLEXPRESS', 'test.dbo');
   
   //MySql Sample
     //Prosper\Query::configure(Prosper\Query::MYSQL_MODE, 'root', 'xamppdevpwd', 'localhost', 'test');
